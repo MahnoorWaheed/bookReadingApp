30b55b09a3aa1d29361466667cc48e65c6c5ccac
MahnoorWaheed <mahnoorwaheed8395@gmail.com>
2021-12-06 23:26:11 +0500

    fetching successful

--- lib/screens/bookview.dart
+++ lib/screens/bookview.dart
@@ -1,6 +1,8 @@
-import 'package:bookapp/screens/bookviewwidgets/listofchap.dart';
-import 'package:bookapp/screens/bookviewwidgets/readbookdetails.dart';
+import 'package:bookapp/models/usermodel.dart';
 import 'package:bookapp/utils/uils.dart';
+import 'package:bookapp/widgets/bookviewwidgets/listofchap.dart';
+import 'package:bookapp/widgets/bookviewwidgets/readbookdetails.dart';
+import 'package:bookapp/widgets/widgetshome/bookcover.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,31 +12,31 @@ class BookView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+   
+    final bookViewvar= ModalRoute.of(context)!.settings.arguments as Book;
     return Scaffold(
       body: Stack(
         clipBehavior: Clip.none,
         children: [
-        BookReadDetails(),
-              Positioned(
-                top: 165,
-                left: 160,
-                child: Container(
-                           height: 60,
-                           width: 35,
-                           child: Center(
-                             child: FaIcon(FontAwesomeIcons.star)),
-                           decoration: BoxDecoration(
-                             borderRadius: BorderRadius.circular(20),
-                             color: Colors.white,
-                           ),
-                         ),
-              ),
-              Positioned(
-                top: 280,
-                left: 20,
-                child: ListOfChap(),
-              ),
-      ],),
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25)
+            ),
+            child: BookCover(bookCover: bookViewvar.bookCover, 
+            fit: BoxFit.cover,
+            width: screenWidth(context),
+            height: screenHeight(context)*0.42,
+            ),
+          ),
+        BookReadDetails(allChapDesc: bookViewvar),
+        Padding(
+          padding: const EdgeInsets.only(top:260, left:18),
+          child: Container(
+            child: ListOfChap(chapdetails: bookViewvar.chapters),),
+        ),
+      ],
+      ),
     );
   }
 }
